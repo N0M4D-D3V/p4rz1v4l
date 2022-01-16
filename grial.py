dfc38ca486c4f08bddc92fefb4891215582e6d71
Victor <victor.gonzalez@kubide.esr>
2022-01-16 02:42:52 +0100

    version and text modified

--- grial.py
+++ grial.py
@@ -1,46 +1,48 @@
+app_version = 'v0.0.0'
 menu_options = {
-        1: 'Backtest',
-        2: 'Genetic Algorithm',
-        3: 'Create new trade-bot',
-        4: 'Run trade-bot! >:v',
-        5: "Go Away ='(",
-        }
+    1: 'Backtest',
+    2: 'Genetic Algorithm',
+    3: 'Create new bot',
+    4: 'Run P4RZ1V4L! >:v',
+    5: "Go Away ='(",
+}
+
 
 def print_menu():
+    print('\n<>----------< P4RZ1V4L >----------<>')
+    print('        -----< ' + app_version + ' >-----\n')
     for key in menu_options.keys():
         print(' <> ' + str(key) + ' >-< ' + menu_options[key])
 
-print('\n<>----------< P4RZ1V4L >----------<>\n')
-print('Console enviroment running...')
 
-while(True):
-    print_menu()
-    print('\n')
-    
-    try:
-        option = int(input(' -> Enter your choice: '))
-    except SyntaxError:
-        print('bad input')
-        option = 0
-    except ValueError:
-        print('bad input')
-        option = 0
+def manage_options(selected_option: int):
+    if selected_option == 1:
+        print("\n   Backtest initialized!")
 
-    if option == 1:
-        print("\nBacktest initialized!")
-    
-    elif option == 2:
-        print("\nGenetic Algorithm initialized!")
+    elif selected_option == 2:
+        print("\n   Genetic Algorithm initialized!")
 
-    elif option == 3:
-        print("\nBot-Creator initialized!")
+    elif selected_option == 3:
+        print("\n   Bot-Creator initialized!")
 
-    elif option == 4:
-        print("\nBot-Runner initialized!")
+    elif selected_option == 4:
+        print("\n   Bot-Runner initialized!")
 
-    elif option == 5:
-        print("\n\nU 4r3 n0t r1ch 3n0ugh! >:[")
+    elif selected_option == 5:
+        print("\n\n   U 4r3 n0t r1ch 3n0ugh! >:[")
         exit()
     else:
-        print("\n404 - Option not found!")
+        print("\n   404 - Option not found!")
 
+
+while True:
+    try:
+        print_menu()
+        print('\n')
+        option = int(input(' -> Enter your choice: '))
+        manage_options(option)
+
+    except SyntaxError:
+        print("\n   404 - Option not found!")
+    except ValueError:
+        print("\n   404 - Option not found!")
