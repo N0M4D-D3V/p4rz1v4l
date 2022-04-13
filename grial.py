722b7e6efa6575adfacc52b3e04eebeb96b4bd59
N0M4D <>
2022-04-13 18:52:28 +0200

    graph module add - classes structures added - not working yet

--- grial.py
+++ grial.py
@@ -4,7 +4,6 @@ from Backtesting.backtester_menu import BacktesterMenu
 from Config.dictionary.grial_config_dictionary import menu_options
 from Utils.text import *
 
-
 genetic_algorithm_submenu = GeneticAlgorithmMenu()
 backtester_submenu = BacktesterMenu()
 
@@ -33,16 +32,17 @@ def manage_options(selected_option):
         print_option_not_found()
 
 
-while True:
-    try:
-        print_menu()
-        print('\n')
-        option = int(input(' -> Enter your choice: '))
-        manage_options(option)
+if __name__ == "__main__":
+    while True:
+        try:
+            print_menu()
+            print('\n')
+            option = int(input(' -> Enter your choice: '))
+            manage_options(option)
 
-    except (SyntaxError, ValueError):
-        print_option_not_found()
-    except KeyboardInterrupt:
-        print_exit()
-    except NetworkError:
-        print_connectivity_error()
+        except (SyntaxError, ValueError):
+            print_option_not_found()
+        except KeyboardInterrupt:
+            print_exit()
+        except NetworkError:
+            print_connectivity_error()
