c94bc9a7e9fcc4d94703b4b77b31ca50a46b2be1
N0M4D <>
2022-07-26 22:22:24 +0200

    some comentaries

--- Abstract/abstract_genetic_algorithm.py
+++ Abstract/abstract_genetic_algorithm.py
@@ -3,10 +3,13 @@ import abc
 
 class AbstractGeneticAlgorithm(metaclass=abc.ABCMeta):
 
+    # Executes the genetic algorithm sentences.
     @abc.abstractmethod
     def run(self):
         raise NotImplementedError
 
+    # Prints the genetic algorithm results related to a
+    # generation number in console.
     @abc.abstractmethod
     def print_result(self, generation_number: int):
         raise NotImplementedError
--- Abstract/abstract_strategy.py
+++ Abstract/abstract_strategy.py
@@ -3,14 +3,17 @@ import abc
 
 class AbstractStrategy(metaclass=abc.ABCMeta):
 
+    # Prepare the strategy. Sets the params in pre-executing sentences.
     @abc.abstractmethod
     def set_up(self, dataframe):
         raise NotImplementedError
 
+    # Checks if long signal is OK.
     @abc.abstractmethod
     def check_long_signal(self, i):
         raise NotImplementedError
 
+    # Checks if short signal is OK.
     @abc.abstractmethod
     def check_short_signal(self, i):
         raise NotImplementedError
