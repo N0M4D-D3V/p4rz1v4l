12edba228065e7f85300156730333db587e8a9d6
CROM <victor.develops@gmail.com>
2023-02-21 18:30:57 +0100

    <> electron added <>

---
+++ android/app/src/main/java/com/p4rz1v4l/MainActivity.java
@@ -0,0 +1,5 @@
+package com.p4rz1v4l;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.p4rz1v4l',
+  appName: 'p4rz1v4l',
+  webDir: 'dist/p4rz1v4l',
+  bundledWebRuntime: false
+};
+
+export default config;
