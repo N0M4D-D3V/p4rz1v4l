4e5dfb0284ce2872829e91a39d88b6fcbaaa1949
CROM <victor.develops@gmail.com>
2023-02-23 18:36:00 +0100

    backtest bad form and g.a. form impl

--- src/app/pages/backtest/backtest.module.ts
+++ src/app/pages/backtest/backtest.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { BacktestRoutingModule } from "./backtest-routing.module";
 import { BacktestPage } from "./backtest.page";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @NgModule({
   declarations: [BacktestPage],
   exports: [BacktestPage],
-  imports: [CommonModule, BacktestRoutingModule],
+  imports: [CommonModule, BacktestRoutingModule, MatFormFieldModule],
   providers: [],
   bootstrap: [],
 })
