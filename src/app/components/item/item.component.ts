2c2d3e89c75ef5da9c9211dcc916e18f20bab590
N0M4D <elcadenas28@gmail.com>
2023-03-18 19:25:19 +0100

    app-item working with objects

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,12 +1,14 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { IndicatorInfo } from "@interfaces/indicator.interface";
+import { NgbPopover } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "app-item",
   templateUrl: "./item.component.html",
   styleUrls: ["./item.component.scss"],
 })
-export class ItemComponent {
-  @Input() label: string;
+export class ItemComponent<T> {
+  @Input() item: IndicatorInfo;
 
   @Output() onDelete: EventEmitter<void> = new EventEmitter<void>();
   @Output() onEdit: EventEmitter<string> = new EventEmitter<string>();
@@ -17,7 +19,8 @@ export class ItemComponent {
     this.onDelete.emit();
   }
 
-  public onEditTouched(): void {
+  public onEditTouched(popover: NgbPopover): void {
+    popover.open(this.item);
     // this.onEdit.emit(this.label);
   }
 }
