65f76cc3fc8f7e5f594299831e31c374ca48d623
N0M4D <elcadenas28@gmail.com>
2023-03-20 12:58:27 +0100

    update form when strategy is edited

--- src/app/pages/backtest/backtest.page.ts
+++ src/app/pages/backtest/backtest.page.ts
@@ -6,7 +6,14 @@ import { StrategyService } from "@services/strategy/strategy.service";
 import { BsModalService } from "ngx-bootstrap/modal";
 import { Observable, Subscription } from "rxjs";
 import { filter } from "rxjs/operators";
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { Exchange } from "ccxt";
+import { ExchangeFactoryService } from "@services/exchange/exchange-factory.service";
 
 @Component({
   selector: "app-backtest",
@@ -16,15 +23,22 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class BacktestPage implements OnInit, OnDestroy {
   private subStrategies: Subscription;
   private subStratID: Subscription;
+  private subExchange: Subscription;
 
+  public exchange: Exchange;
+
+  public strategies$: Observable<Strategy[]>;
   public strategies: Strategy[];
   public exchangeList: string[] = [];
   public form: FormGroup;
   public isEditAvailable: boolean = false;
 
+  public markets: string[];
+
   constructor(
     private readonly modalService: BsModalService,
     private readonly fb: FormBuilder,
+    private readonly exchangeFactory: ExchangeFactoryService,
     private readonly exchangeService: ExchangeService,
     private readonly strategyService: StrategyService
   ) {}
@@ -35,19 +49,13 @@ export class BacktestPage implements OnInit, OnDestroy {
     this.exchangeList = this.exchangeService.getExchanges();
   }
 
-  private createForm(): void {
-    this.form = this.fb.group({
-      stratID: [null, Validators.required],
-      exchange: ["", Validators.required],
-      market: ["", Validators.required],
-    });
-  }
-
   private initSubscriptions(): void {
-    this.subStrategies = this.strategyService
+    this.strategies$ = this.strategyService
       .getObservable()
-      .pipe(filter((res: Strategy[]) => !!res))
-      .subscribe((strategies: Strategy[]) => (this.strategies = strategies));
+      .pipe(filter((res: Strategy[]) => !!res));
+    this.subStrategies = this.strategies$.subscribe((strategies: Strategy[]) =>
+      this.onStrategies(strategies)
+    );
 
     this.subStratID = this.form
       .get("stratID")
@@ -56,15 +64,39 @@ export class BacktestPage implements OnInit, OnDestroy {
         this.strategyService.selectedStrategy = this.strategies[index];
         this.isEditAvailable = true;
       });
+
+    this.subExchange = this.form
+      .get("exchange")
+      .valueChanges.pipe(filter((name: string) => !!name))
+      .subscribe(async (name: string) => {
+        this.form.get("market").disable();
+        this.exchange = this.exchangeFactory.getInstance(name);
+        await this.exchange.loadMarkets();
+        this.form.get("market").enable();
+      });
+  }
+
+  public onStrategies(strategies: Strategy[]): void {
+    this.strategies = strategies;
+    this.form.get("stratID").setValue(null);
   }
 
   public onEdit(): void {
     this.modalService.show(EditStrategyModal);
   }
 
+  private createForm(): void {
+    this.form = this.fb.group({
+      stratID: [null, Validators.required],
+      exchange: ["", Validators.required],
+      market: [{ value: "", disabled: true }, Validators.required],
+    });
+  }
+
   ngOnDestroy(): void {
     this.strategyService.clearSelectedStrategy();
     this.subStrategies.unsubscribe();
     this.subStratID.unsubscribe();
+    this.subExchange.unsubscribe();
   }
 }
