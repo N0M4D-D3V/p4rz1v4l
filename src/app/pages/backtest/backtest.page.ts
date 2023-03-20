aee4abd6402e2cdc3e49fdc245f60a9b608502e9
N0M4D <elcadenas28@gmail.com>
2023-03-20 10:58:22 +0100

    receiving strategies data in backtest page

--- src/app/pages/backtest/backtest.page.ts
+++ src/app/pages/backtest/backtest.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import { Strategy } from "@interfaces/strategies.interface";
 import { EditStrategyModal } from "@modals/edit-strategy/edit-strategy.modal";
 import { ExchangeService } from "@services/exchange/exchange.service";
+import { StrategyService } from "@services/strategy/strategy.service";
 import { BsModalService } from "ngx-bootstrap/modal";
+import { Observable } from "rxjs";
+import { filter } from "rxjs/operators";
 
 @Component({
   selector: "app-backtest",
@@ -9,15 +13,24 @@ import { BsModalService } from "ngx-bootstrap/modal";
   styleUrls: ["./backtest.page.scss"],
 })
 export class BacktestPage implements OnInit {
+  public strategiesObs$: Observable<Strategy[]>;
   public exchangeList: string[] = [];
 
   constructor(
     private readonly modalService: BsModalService,
-    private readonly exchangeService: ExchangeService
+    private readonly exchangeService: ExchangeService,
+    private readonly strategyService: StrategyService
   ) {}
 
   ngOnInit(): void {
+    this.initSubscriptions();
     this.exchangeList = this.exchangeService.getExchanges();
+  }
+
+  private initSubscriptions(): void {
+    this.strategiesObs$ = this.strategyService
+      .getObservable()
+      .pipe(filter((res: Strategy[]) => !!res));
   }
 
   public onEdit(): void {
