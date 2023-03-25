8168ce0ff5582ac30b6c77c23ab0ce4878d19642
Juan Carlos Arraez <juancarlos@MacBook-Air-de-Juan.local>
2023-03-25 20:08:08 +0100

    fixes husky

--- src/app/core/cicd/deploy-package-json-cicd-tag.sh
+++ src/app/core/cicd/deploy-package-json-cicd-tag.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 action=$1 # patch, minor or major
 
+# Elevando versión
 npm version "$action"
+
 # Get app version
 tag=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' package.json)
+
