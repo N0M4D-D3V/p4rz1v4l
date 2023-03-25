53bfc4f3cc77d7cc35a78bc01e7957ac2ed88fea
Juan Carlos Arraez <juancarlos@MacBook-Air-de-Juan.local>
2023-03-25 20:59:44 +0100

    CI/CD -> Create patch for patch

--- src/app/core/cicd/deploy-package-json-cicd-tag.sh
+++ src/app/core/cicd/deploy-package-json-cicd-tag.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+if [ -z "$VERSION_EXECUTED" ]; then
+  echo "El script de versión no se ejecutó en este commit."
+  exit 1
+fi
+
 action=$1 # patch, minor or major
 
-# Elevando versión
+git add -A
+git commit -m "CI/CD -> Create patch for $action"
 npm version "$action"
-
 # Get app version
 tag=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' package.json)
-
+# Upload git changes to remote
+git push
+git push origin "v$tag"
