bd5878bd7d06c7c1443e4223ebe2f3eb76e62a37
Juan Carlos Arraez <juancarlos@MacBook-Air-de-Juan.local>
2023-03-25 22:37:04 +0100

    Fix test 7

--- src/app/core/cicd/deploy-package-json-cicd-tag.sh
+++ src/app/core/cicd/deploy-package-json-cicd-tag.sh
@@ -7,10 +7,9 @@ action=$1 # patch, minor or major
 tag=$(awk -F \" '/"version": ".+"/ { print $4; exit; }' package.json)
 
 git add -A
-git pull origin $branch
+git fetch origin $branch
 git merge --no-ff -m "CI/CD -> Create patch for $action" origin/$branch
 npm version "$action"
 # Get app version
 # Upload git changes to remote
-git push
-git push origin "v$tag"
+git push --follow-tags
