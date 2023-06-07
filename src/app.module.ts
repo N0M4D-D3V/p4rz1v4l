4f54e344bd59de660b3a7b06bffc56ecb85538d0
N0M4D <victor.develops@gmail.com>
2023-06-07 06:08:08 +0200

    typeORM integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "p4rz1v4l",
+      password: "be_rich_or_be_poor",
+      database: "p4rz1v4l",
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
